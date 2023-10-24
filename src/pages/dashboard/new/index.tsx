db08def6783c2503ea41c8abb74e6233a2f4ee13
José Eduardo Arnhold <duduarnhold2@gmail.com>
2023-10-24 09:21:52 -0300

    FEAT: adicionando imagem na aplicação e add função de excluir imagem

--- src/pages/dashboard/new/index.tsx
+++ src/pages/dashboard/new/index.tsx
@@ -2,7 +2,7 @@ import { Container } from "../../../components/container"
 
 import { DashboardHeader } from "../../../components/panelHeader"
 
-import { FiUpload } from "react-icons/fi";
+import { FiUpload, FiTrash } from "react-icons/fi";
 
 import { useForm } from "react-hook-form";
 
@@ -47,6 +47,13 @@ const schema = z.object({
 
 type FormData = z.infer<typeof schema>;
 
+interface ImageItemProps{
+    uid: string;
+    name: string;
+    previewUrl: string;
+    url: string;
+}
+
 export function New() {
 
     const { user } = useContext(AuthContext)
@@ -56,6 +63,8 @@ export function New() {
         resolver: zodResolver(schema),
         mode: "onChange"
     })
+
+    const [ carImages, setCarImages ] = useState<ImageItemProps[]>([]);
 
     function onSubmit(data: FormData){
         console.log(data)
@@ -88,9 +97,30 @@ export function New() {
         uploadBytes(uploadRef, image)
         .then((snapshot) => {
             getDownloadURL(snapshot.ref).then((downloadUrl) => {
-                console.log(downloadUrl)
+                const imageItem = {
+                    name: uidImage,
+                    uid: currentUid,
+                    previewUrl: URL.createObjectURL(image),
+                    url: downloadUrl,
+                }
+                setCarImages((images) => [
+                    ...images, imageItem
+                ])
             })
         })
+
+    }
+
+    async function handleDeleteImage(item: ImageItemProps){
+        const imagePath = `images/${item.uid}/${item.name}`;
+        const imageRef = ref(storage, imagePath);
+
+        try{
+            await deleteObject(imageRef);
+            setCarImages(carImages.filter((car) => car.url !== item.url));
+        }catch(err){
+            console.log("Erro ao Deletar imagem")
+        }
 
     }
 
@@ -120,6 +150,23 @@ export function New() {
                     </div>
                     
                 </button>
+
+                {carImages.map( item => (
+                    <div key={item.name} className="w-full h-32 flex items-center justify-center relative">
+                        <button className="absolute" onClick={() => handleDeleteImage(item)}>
+                            <FiTrash
+                                size={28}
+                                color="#FFF"
+                            />
+                        </button>
+                        <img 
+                            src={item.previewUrl} 
+                            className="rounded-lg w-full h-32 object-cover"
+                            alt="Foto do carro"
+                        />
+
+                    </div>
+                ))}
             </div>
 
 
