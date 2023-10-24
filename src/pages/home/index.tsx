524a6552b02980391f4e1b8e92fc3f75f9015d87
José Eduardo Arnhold <duduarnhold2@gmail.com>
2023-10-24 10:47:04 -0300

    Mostrando carros na home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,77 @@
+import { useState, useEffect } from "react";
+
 import {Container} from "../../components/container"
 
+import { Link } from "react-router-dom";
+
+import {
+    collection,
+    query,
+    getDocs,
+    orderBy,
+} from "firebase/firestore";
+
+import {
+ db,
+} from "../../services/firebaseconnection";
+
+
+interface CarProps{
+    id: string;
+    name: string;
+    year: string;
+    uid: string;
+    price: string | number;
+    city: string;
+    km: string;
+    images: carImagesProps[];
+}
+
+interface carImagesProps{
+    name: string;
+    uid: string;
+    url: string;
+}
+
 export function Home() {
+
+        const [cars, setCars] = useState<CarProps[]>([]);
+
+        useEffect (() => {
+            async function loadCars(){
+                const carsRef = collection(db, "cars")
+                const queryRef = query(carsRef, orderBy("created", "desc"))
+                getDocs(queryRef)
+                .then((snapshot) => {
+                    const listCars = [] as CarProps[];
+                        snapshot.forEach(doc => {
+                            listCars.push({
+                                id: doc.id,
+                                name: doc.data().name,
+                                year: doc.data().year,
+                                uid: doc.data().uid,
+                                price: doc.data().price,
+                                city: doc.data().city,
+                                km: doc.data().km,
+                                images: doc.data().images,
+                            })
+                        })
+
+                        setCars(listCars);
+
+
+
+
+                })
+                .catch((error) => {
+                    console.log("Erro ao carregar carro do banco de dados ")
+                    console.log(error)
+                })
+            }
+
+            loadCars();
+        }, [])
+
     return (
       <Container>
         <section className="bg-white p-4 rounded-lg w-full max-w-3xl mx-auto flex justify-center items-center gap-2">
@@ -20,25 +91,33 @@ export function Home() {
 
 
         <main className=" grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
-            <section className=" w-full bg-white rounded-lg">
+            {cars.map(car => (
+                <Link key={car.id} to={`/car/${car.id}`}>
+                <section  className=" w-full bg-white rounded-lg" >
                 <img 
                     className="w-full rounded-lg mb-2 max-h-72 hover:scale-105 transition-all"
-                    src="https://image.webmotors.com.br/_fotos/anunciousados/gigante/2023/202310/20231013/fiat-cronos-1.3-firefly-flex-drive-manual-wmimagem11444130345.jpg?s=fill&w=552&h=414&q=60" alt="Carro" />
-                <p className="font-bold mt-1 mb-2 px-2">BMW 320i</p>
+                    src={car.images[0].url} alt="Carro" />
+                <p className="font-bold mt-1 mb-2 px-2">{car.name}</p>
                 <div className="flex flex-col px-2">
-                    <span className="text-zinc-700 mb-6 ">Ano 2016/2016 | 23.000Km</span>
+                    <span className="text-zinc-700 mb-6 ">{car.year} | {car.km}</span>
                     <strong className="text-black font-medium text-xl">
-                        R$ 120.000
+                        {car.price}
                     </strong>
                 </div>
 
                 <div className="w-full h-px bg-slate-300 my-2"></div>
 
                 <div className="px-2 pb-2">
-                    <span className="text-zinc-700">Campo Grande - MS</span>
+                    <span className="text-zinc-700">{car.city}</span>
                 </div>
             
             </section>
+                
+                
+                
+                
+                </Link>
+            ))}
         </main>
       </Container>
         
