3d8132fa691cd7df362f239418b0f446a0fa859e
José Eduardo Arnhold <duduarnhold2@gmail.com>
2023-10-26 14:58:43 -0300

    Buscando carros do usuário no db e os mostrando na tela

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -7,7 +7,7 @@ import { DashboardHeader } from "../../components/panelHeader";
 
 import { FiTrash2 } from "react-icons/fi";
 
-import { collection, getDocs, query, where } from "firebase/firestore";
+import { collection, getDocs, query, where, doc, deleteDoc} from "firebase/firestore";
 
 import { db, storage } from "../../services/firebaseconnection";
 
@@ -79,6 +79,12 @@ export function Dashboard() {
         loadCars();
     }, [user])
 
+    async function handleDeleteCar(id: string){
+        const docRef = doc(db, "cars", id);
+        await deleteDoc(docRef);
+        setCars(cars.filter(car => car.id !== id))
+    }
+
 
 
 
@@ -88,10 +94,11 @@ export function Dashboard() {
             <DashboardHeader/>
 
             <main className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
-                <section className="w-full bg-white rounded-lg relative">
+                {cars.map( car => (
+                    <section className="w-full bg-white rounded-lg relative" key={car.id}>
 
                     <button 
-                    onClick={ () => {} }
+                    onClick={ () => { handleDeleteCar(car.id) } }
                     className="absolute bg-white w-14 h-14 rounded-full flex items-center justify-center
                     right-2 top-2 drop-shadow
                     ">
@@ -99,28 +106,29 @@ export function Dashboard() {
                     </button>
                     <img
                         className=" w-full rounded-lg mb-2 max-h-70"
-                        src="https://firebasestorage.googleapis.com/v0/b/webcarros2.appspot.com/o/images%2FBCIJGK2NIWSyigsUGEdo0A1RKRu2%2F3c31d1b0-497c-466f-a693-711bcaa3d564?alt=media&token=514b482b-460b-4d5a-8004-b9457be4d700"
+                        src={car.images[0].url}
 
                     />
                     <p className="font-bold mt-1 px-2 mb-2">
-                        Nissan Versa
+                        {car.name}
                     </p>
                     <div className="flex flex-col px-2">
                         <span className="text-zinc-700">
-                            Ano 2010/2010   |   230.000km
+                            {car.year}   |   {car.km}
                         </span>
                         <strong className="text-black font-bold mt-4">
-                            R$ 150.000
+                            {car.price}
                         </strong>
 
                     </div>
 
                     <div className="w-full h-px bg-slate-200 my-2"></div>
                     <div className="px-2 pb-2">
-                        <span className="text-black">Campo Grande - MS</span>
+                        <span className="text-black">{car.city}</span>
                     </div>
 
                 </section>
+                ))}
 
             </main>
             
